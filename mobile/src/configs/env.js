a4611e858389a51c5f057e107872dad673820e3e
GuiFullSoft <guilherme.fontana@fullsoft.com.br>
2022-07-12 21:55:48 -0300

    env.js

--- mobile/src/configs/env.js
+++ mobile/src/configs/env.js
@@ -1,10 +1,6 @@
 module.exports = {
     env: {
-        API_URL: 'http://172.25.100.95',
+        API_URL: 'http://',
         API_PORT: ':3333'
     },
   }
-
-
-// API_URL=http://192.168.0.56
-// API_URL=http://172.25.100.95
